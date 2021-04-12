4f4995dd0e9ad7afe2397a08c0b6ea7d432346fc
jakob <jakob.willforss@hotmail.com>
2021-04-12 21:58:13 +0200

    Basic movement, sprite flipping and jumping in place

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,22 +6,49 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 28f;
 
+    // State
+    private bool isAlive = true;
+
+    // Cached component references
     private Rigidbody2D myRigidBody;
+    private Animator myAnimator;
 
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Run();
+        Jump();
+        FlipSprite();
     }
 
     private void Run() {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+
+        bool playerHorizSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHorizSpeed);
+    }
+
+    private void Jump() {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void FlipSprite() {
+        bool playerHorizSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHorizSpeed) {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1);
+        }
     }
 }
