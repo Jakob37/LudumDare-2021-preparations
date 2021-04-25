71746269c92d78e67fa70c9dbe2fff2ddb3619a9
jakob <jakob.willforss@hotmail.com>
2021-04-25 12:32:31 +0200

    Meta files and jellyfish

--- Assets/Jellyfish.cs
+++ Assets/Jellyfish.cs
@@ -4,29 +4,18 @@ using UnityEngine;
 
 public class Jellyfish : MonoBehaviour
 {
-    [SerializeField] public float detectionRange;
-    [SerializeField] public float moveSpeed;
-
-    private Player player;
-    private Transform myTransform;
 
     void Start()
     {
-        player = FindObjectOfType<Player>();
-        myTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector2.Distance(myTransform.position, player.transform.position);
-        if (distance < detectionRange) {
-            MoveTowardsPlayer();
-        }
+
     }
 
     private void MoveTowardsPlayer() {
-        Vector3 direction = (myTransform.position - player.transform.position).normalized;
-        myTransform.position -= direction * moveSpeed * Time.deltaTime * 60;
+
     }
 }
--- Assets/Shark.cs
+++ Assets/Shark.cs
@@ -4,36 +4,13 @@ using UnityEngine;
 
 public class Shark : MonoBehaviour
 {
-    // [SerializeField] public float detectionRange;
-    // [SerializeField] public float moveSpeed;
-
-    // private Player player;
-    // private Transform myTransform;
-    // private EnemyHealth health;
 
     void Start()
     {
-        // player = FindObjectOfType<Player>();
-        // myTransform = GetComponent<Transform>();
-        // health = GetComponent<EnemyHealth>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // float distance = Vector2.Distance(myTransform.position, player.transform.position);
-        // if (distance < detectionRange) {
-        //     MoveTowardsPlayer();
-        // }
-    }
 
-    // private void MoveTowardsPlayer() {
-    //     Vector3 direction = (myTransform.position - player.transform.position).normalized;
-    //     float usedSpeed = moveSpeed;
-    //     if (health.GetIsInjured()) {
-    //         usedSpeed = moveSpeed * 0.5f;
-    //     }
-    //     myTransform.position -= direction * usedSpeed;
-    //     transform.localScale = new Vector2(-Mathf.Sign(direction.x), 1);
-    // }
+    }
 }
